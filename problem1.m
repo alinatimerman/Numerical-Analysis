274ae53c161921af5b4e64977f15418c2d9323c2
Alina Timerman <79532163+alinatimerman@users.noreply.github.com>
2021-02-27 17:23:23 +0200

    Add files via upload

---
+++ problem1.m
@@ -0,0 +1,19 @@
+﻿
+
+n=input('number')
+ok=0;
+for i=2:sqrt(n)
+    if rem(n,i)==0
+        ok=1;
+
+end
+end
+
+if ok==1   
+    display('not prime')
+elseif ok==0
+    display('prime')
+else n==2
+    display('prime')
+
+end
